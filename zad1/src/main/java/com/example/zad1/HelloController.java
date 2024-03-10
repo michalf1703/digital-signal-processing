b632a969e773c0ff5f1dc2f647e4d838abea3c9a
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2024-03-10 03:48:43 +0100

    add new fields

--- zad1/src/main/java/com/example/zad1/HelloController.java
+++ zad1/src/main/java/com/example/zad1/HelloController.java
@@ -14,6 +14,14 @@ public class HelloController {
     private ChoiceBox<String> rodzajSygnalu;
     @FXML
     private TextField amplitudaF, czasTrwaniaF, okresPodstawowyF, poczatkowyF, wspolczynnikWypelnieniaF, okresF;
+    @FXML
+    private TextField czasSkokuF;
+    @FXML
+    private TextField czestoscProbkowaniaF;
+    @FXML
+    private TextField numerProbkiSkokuF;
+    @FXML
+    private TextField prawdopodobienstwoF;
 
     @FXML
     private ChoiceBox<String> wybierzOperacje;
@@ -48,11 +56,14 @@ public class HelloController {
     @FXML
     protected void initialize() {
         rodzajSygnalu.getItems().addAll(opcje);
-        rodzajSygnalu.setValue(opcje[0]); // Ustaw domyślną wartość
         wybierzOperacje.getItems().addAll(opcjeOperacje);
         przedzialHistogramu.getItems().addAll(opcjePrzedzial);
+        rodzajSygnalu.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            checkSignal();
+        });
         checkSignal();
     }
+
 
 
     void checkSignal() {
@@ -64,66 +75,110 @@ public class HelloController {
                 czasTrwaniaF.setDisable(false);
                 okresPodstawowyF.setDisable(true);
                 wspolczynnikWypelnieniaF.setDisable(true);
+                czasSkokuF.setDisable(true);
+                czestoscProbkowaniaF.setDisable(true);
+                numerProbkiSkokuF.setDisable(true);
+                prawdopodobienstwoF.setDisable(true);
             } else if (signal.equals("szum gaussowski")) {
                 amplitudaF.setDisable(false);
                 poczatkowyF.setDisable(false);
                 czasTrwaniaF.setDisable(false);
                 okresPodstawowyF.setDisable(true);
                 wspolczynnikWypelnieniaF.setDisable(true);
+                czasSkokuF.setDisable(true);
+                czestoscProbkowaniaF.setDisable(true);
+                numerProbkiSkokuF.setDisable(true);
+                prawdopodobienstwoF.setDisable(true);
             } else if (signal.equals("sygnał sinusoidalny")) {
                 amplitudaF.setDisable(false);
                 poczatkowyF.setDisable(false);
                 czasTrwaniaF.setDisable(false);
                 okresPodstawowyF.setDisable(false);
                 wspolczynnikWypelnieniaF.setDisable(true);
+                czasSkokuF.setDisable(true);
+                czestoscProbkowaniaF.setDisable(true);
+                numerProbkiSkokuF.setDisable(true);
+                prawdopodobienstwoF.setDisable(true);
             } else if (signal.equals("sygnał sinusoidalny wyprostowany jednopołówkowo")) {
                 amplitudaF.setDisable(false);
                 poczatkowyF.setDisable(false);
                 czasTrwaniaF.setDisable(false);
                 okresPodstawowyF.setDisable(false);
                 wspolczynnikWypelnieniaF.setDisable(true);
+                czasSkokuF.setDisable(true);
+                czestoscProbkowaniaF.setDisable(true);
+                numerProbkiSkokuF.setDisable(true);
+                prawdopodobienstwoF.setDisable(true);
             } else if (signal.equals("sygnał sinusoidalny wyprostowany dwupołówkowo")) {
                 amplitudaF.setDisable(false);
                 poczatkowyF.setDisable(false);
                 czasTrwaniaF.setDisable(false);
                 okresPodstawowyF.setDisable(false);
                 wspolczynnikWypelnieniaF.setDisable(true);
+                czasSkokuF.setDisable(true);
+                czestoscProbkowaniaF.setDisable(true);
+                numerProbkiSkokuF.setDisable(true);
+                prawdopodobienstwoF.setDisable(true);
             } else if (signal.equals("sygnał prostokątny")) {
                 amplitudaF.setDisable(false);
                 poczatkowyF.setDisable(false);
                 czasTrwaniaF.setDisable(false);
                 okresPodstawowyF.setDisable(false);
                 wspolczynnikWypelnieniaF.setDisable(false);
+                czasSkokuF.setDisable(true);
+                czestoscProbkowaniaF.setDisable(true);
+                numerProbkiSkokuF.setDisable(true);
+                prawdopodobienstwoF.setDisable(true);
             } else if (signal.equals("sygnał prostokątny symetryczny")) {
                 amplitudaF.setDisable(false);
                 poczatkowyF.setDisable(false);
                 czasTrwaniaF.setDisable(false);
                 okresPodstawowyF.setDisable(false);
                 wspolczynnikWypelnieniaF.setDisable(false);
+                czasSkokuF.setDisable(true);
+                czestoscProbkowaniaF.setDisable(true);
+                numerProbkiSkokuF.setDisable(true);
+                prawdopodobienstwoF.setDisable(true);
             } else if (signal.equals("sygnał trójkątny")) {
                 amplitudaF.setDisable(false);
                 poczatkowyF.setDisable(false);
                 czasTrwaniaF.setDisable(false);
                 okresPodstawowyF.setDisable(false);
                 wspolczynnikWypelnieniaF.setDisable(false);
+                czasSkokuF.setDisable(true);
+                czestoscProbkowaniaF.setDisable(true);
+                numerProbkiSkokuF.setDisable(true);
+                prawdopodobienstwoF.setDisable(true);
             } else if (signal.equals("skok jednostkowy")) {
                 amplitudaF.setDisable(false);
                 poczatkowyF.setDisable(false);
                 czasTrwaniaF.setDisable(false);
                 okresPodstawowyF.setDisable(true);
                 wspolczynnikWypelnieniaF.setDisable(true);
+                czasSkokuF.setDisable(false);
+                czestoscProbkowaniaF.setDisable(true);
+                numerProbkiSkokuF.setDisable(true);
+                prawdopodobienstwoF.setDisable(true);
             } else if (signal.equals("impuls jednostkowy")) {
                 amplitudaF.setDisable(false);
                 poczatkowyF.setDisable(false);
-                czasTrwaniaF.setDisable(true);
+                czasTrwaniaF.setDisable(false);
                 okresPodstawowyF.setDisable(true);
                 wspolczynnikWypelnieniaF.setDisable(true);
+                czasSkokuF.setDisable(true);
+                czestoscProbkowaniaF.setDisable(false);
+                numerProbkiSkokuF.setDisable(false);
+                prawdopodobienstwoF.setDisable(true);
             } else if (signal.equals("szum impulsowy")) {
                 amplitudaF.setDisable(false);
                 poczatkowyF.setDisable(false);
                 czasTrwaniaF.setDisable(false);
                 okresPodstawowyF.setDisable(true);
                 wspolczynnikWypelnieniaF.setDisable(true);
+                czasSkokuF.setDisable(true);
+                czestoscProbkowaniaF.setDisable(false);
+                numerProbkiSkokuF.setDisable(true);
+                prawdopodobienstwoF.setDisable(false);
             }
         }
     }
